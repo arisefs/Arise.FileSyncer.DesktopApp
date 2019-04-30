82dc2d8e83de468008fea212261248d663de398a
Csányi István <csanyistvan02@gmail.com>
2019-04-30 14:15:30 +0200

    Changed Log output Updated packages

--- Arise.FileSyncer.Service/Program.cs
+++ Arise.FileSyncer.Service/Program.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Threading;
+using Arise.FileSyncer.Core;
 
 namespace Arise.FileSyncer.Service
 {
@@ -7,6 +8,12 @@ namespace Arise.FileSyncer.Service
     {
         static void Main(string[] args)
         {
+            Log.Error = (msg)=> { Console.WriteLine($"E: {msg}"); };
+            Log.Warning = (msg) => { Console.WriteLine($"W: {msg}"); };
+            Log.Info = (msg) => { Console.WriteLine($"I: {msg}"); };
+            Log.Verbose = (msg) => { Console.WriteLine($"V: {msg}"); };
+            Log.Debug = (msg) => { Console.WriteLine($"D: {msg}"); };
+
             var syncerService = new SyncerService();
             syncerService.Run();
             Thread.Sleep(Timeout.Infinite);
