84aeef73af8461602d414cd55a3d91999eaaed87
Csányi István <csanyistvan02@gmail.com>
2019-06-14 12:39:23 +0200

    updated to latest dotnet

--- Arise.FileSyncer.Service/package.bat
+++ Arise.FileSyncer.Service/package.bat
@@ -5,6 +5,6 @@ if exist "bin\Publish" (
 )
 
 :: Build the self-contained packages
-call dotnet publish -c Release -f netcoreapp3.0 -o ./bin/Publish/win-x64/ --self-contained -r win-x64 || PAUSE
-call dotnet publish -c Release -f netcoreapp3.0 -o ./bin/Publish/win-x86/ --self-contained -r win-x86 || PAUSE
+call dotnet publish -c Release -f netcoreapp3.0 -o ./bin/Publish/win-x64/ --self-contained -r win-x64 -p:PublishReadyToRun=true || PAUSE
+call dotnet publish -c Release -f netcoreapp3.0 -o ./bin/Publish/win-x86/ --self-contained -r win-x86 -p:PublishReadyToRun=true || PAUSE
 call dotnet publish -c Release -f netcoreapp3.0 -o ./bin/Publish/linux-x64/ --self-contained -r linux-x64 || PAUSE
