090a3896ea7ff4bd930d41dde5aa5adb3ad2ecb5
Csányi István <csanyistvan02@gmail.com>
2022-05-18 09:13:27 +0200

    update deps

--- Arise.FileSyncer.TauriUI/src/main.tsx
+++ Arise.FileSyncer.TauriUI/src/main.tsx
@@ -2,7 +2,7 @@ import initUpdater from "./app-data/app-data-updater"
 import initIpcEvents from "./ipc/ipcEvents"
 
 import React from "react"
-import ReactDOM from "react-dom"
+import ReactDOM from "react-dom/client"
 import WindowDisplay from "./window/window-display"
 
 import "./fonts.css"
@@ -12,9 +12,11 @@ async function main() {
     await initUpdater()
     await initIpcEvents()
 
-    ReactDOM.render(<React.StrictMode>
-        <WindowDisplay />
-    </React.StrictMode>, document.getElementById("root"))
+    ReactDOM.createRoot(document.getElementById('root')!).render(
+        <React.StrictMode>
+            <WindowDisplay />
+        </React.StrictMode>
+    )
 }
 
 main()
